d911b6b51b66142d649cad870ddad7cc8504bb9f
Uljad Berdica <ub352@nyu.edu>
2021-12-05 18:45:37 -0500

    read plain

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def read():
     Route for GET requests to the read page.
     Displays some information for the user with links to other pages.
     """
-    docs = db.posts.find({})# sort in descending order of created_at timestamp
+    docs = db.posts.find()# sort in descending order of created_at timestamp
     return render_template('read.html', docs=docs) # render the read template
 
 
