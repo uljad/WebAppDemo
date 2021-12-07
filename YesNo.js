1635d8b6e4cfc23db111846868743aeea2cad2d0
Uljad Berdica <ub352@nyu.edu>
2021-12-07 05:02:16 -0500

    resolved

--- YesNo.js
+++
@@ -1,7 +0,0 @@
-function yesnoCheck() {
-    if (document.getElementById('yesCheck').checked) {
-        document.getElementById('ifYes').style.visibility = 'visible';
-    }
-    else document.getElementById('ifYes').style.visibility = 'hidden';
-
-}
